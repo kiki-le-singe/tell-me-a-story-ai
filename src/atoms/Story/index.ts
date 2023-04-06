f0b4a4fb9eeee78386dc88b277b1266badc2e692
anthony <aalbertini95@gmail.com>
2023-04-06 17:40:41 +0200

    remove useless comment

--- src/atoms/Story/index.ts
+++ src/atoms/Story/index.ts
@@ -10,7 +10,7 @@ const defaultValue: TStory = {
 
 const storyState = atom({
   key: 'storyState',
-  default: defaultValue, // default value (aka initial value)
+  default: defaultValue,
 });
 
 export {storyState};
