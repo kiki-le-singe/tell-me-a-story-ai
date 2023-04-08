a5902b785766290629034a41643fc8adae755e40
anthony <aalbertini95@gmail.com>
2023-04-08 12:19:21 +0200

    Implement openai createImage and display them

--- src/screens/Story/index.tsx
+++ src/screens/Story/index.tsx
@@ -5,92 +5,95 @@ import {
   ScrollView,
   ActivityIndicator,
   ImageBackground,
+  View,
 } from 'react-native';
 import {useRecoilValue} from 'recoil';
 import Config from 'react-native-config';
 import {Configuration, OpenAIApi} from 'openai';
+import FastImage from 'react-native-fast-image';
 
 import {StoryScreenProps} from '../../routes/types';
 import {storyState} from '../../atoms/Story';
 import colors from '../../utils/colors';
-
 import imageWaiting from '../../assets/images/waiting.jpg';
+import {ImagesState, defaultState} from './types';
+
+const configuration = new Configuration({
+  apiKey: Config.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 function StoryScreen({navigation}: StoryScreenProps): JSX.Element {
   const story = useRecoilValue(storyState);
   const [isLoading, setIsLoading] = React.useState(false);
   const [message, setMessage] = React.useState<string | undefined>('');
+  const [images, setImages] = React.useState<ImagesState[]>([defaultState]);
 
-  // React.useEffect(() => {
-  //   console.log(
-  //     '------------------------------------------------------------------------------------------------',
-  //   );
+  const fetchImagesStory = React.useCallback(async (prompt: string = '') => {
+    try {
+      const response = await openai.createImage({
+        prompt,
+        n: 5,
+        size: '256x256',
+      });
 
-  //   const fetchData = async () => {
-  //     const configuration = new Configuration({
-  //       apiKey: Config.OPENAI_API_KEY,
-  //     });
-  //     const openai = new OpenAIApi(configuration);
+      setImages(response.data.data);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('error:', error);
+        console.log('error.message:', error.message);
+      }
+    }
+  }, []);
 
-  //     console.log('configuration:', configuration);
+  const fetchStory = React.useCallback(async () => {
+    const content = `Tell me a story with ${story.hero} as a Hero and ${story.villain} as villain. This story should take place ${story.place}`;
 
-  //     try {
-  //       const response = await openai.createImage({
-  //         prompt: 'a white siamese cat',
-  //         n: 5,
-  //         size: '256x256',
-  //         user: Config.OPENAI_USER_IDENTIFIER,
-  //       });
-  //       console.log('response.data', response.data);
+    try {
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{role: 'user', content}],
+      });
 
-  //       const imageUrl = response.data.data[0].url;
-  //       console.log('imageUrl', imageUrl);
-  //     } catch (error) {
-  //       if (error instanceof Error) {
-  //         console.log('error:', error);
-  //         console.log('error.message:', error.message);
-  //       }
-  //     }
-  //   };
+      const _message = completion.data.choices[0].message;
 
-  //   fetchData();
-  // }, []);
+      setMessage(_message?.content);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('error:', error);
+        console.log('error.message:', error.message);
+      }
+    }
+  }, [story]);
 
   React.useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-
-      const content = `Tell me a story with ${story.hero} as a Hero and ${story.villain} as villain. This story should take place ${story.place}`;
-      const configuration = new Configuration({
-        apiKey: Config.OPENAI_API_KEY,
-      });
-      const openai = new OpenAIApi(configuration);
-
-      try {
-        const completion = await openai.createChatCompletion({
-          model: 'gpt-3.5-turbo',
-          messages: [{role: 'user', content}],
-        });
-
-        console.log('completion', completion);
-
-        const _message = completion.data.choices[0].message;
-        console.log('message', _message?.content);
-        setMessage(_message?.content);
-      } catch (error) {
-        if (error instanceof Error) {
-          console.log('error:', error);
-          console.log('error.message:', error.message);
-        }
-      } finally {
-        setIsLoading(false);
-      }
+      await fetchStory();
+      await fetchImagesStory(
+        `${story.hero} as a Hero and ${story.villain} as villain, ${story.place}`,
+      );
+      setIsLoading(false);
     };
 
     if (story) {
       fetchData();
     }
-  }, [story]);
+  }, [story, fetchStory, fetchImagesStory]);
+
+  function renderImages(): JSX.Element {
+    const _images = images.map((element, index) => (
+      <FastImage
+        key={`Images_${index}`}
+        style={styles.image}
+        source={{
+          uri: element.url,
+        }}
+      />
+    ));
+
+    return <View style={styles.imageContainer}>{_images}</View>;
+  }
 
   return (
     <ScrollView
@@ -108,11 +111,14 @@ function StoryScreen({navigation}: StoryScreenProps): JSX.Element {
         <ImageBackground
           source={imageWaiting}
           resizeMode="cover"
-          style={styles.image}>
+          style={styles.imageLoader}>
           <ActivityIndicator color={colors.BLUE_DARK} size="large" />
         </ImageBackground>
       ) : (
-        <Text style={styles.story}>{message}</Text>
+        <>
+          <Text style={styles.story}>{message}</Text>
+          {renderImages()}
+        </>
       )}
     </ScrollView>
   );
@@ -142,11 +148,23 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
   },
-  image: {
+  imageLoader: {
     flex: 1,
     justifyContent: 'center',
     height: 300,
     width: 'auto',
+  },
+  imageContainer: {
+    marginTop: 40,
+    alignItems: 'center',
+    gap: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 200,
+    borderWidth: 2,
+    borderColor: colors.ORANGE,
   },
 });
 
---
+++ src/screens/Story/types/index.tsx
@@ -0,0 +1,7 @@
+export type ImagesState = {
+  url?: string;
+};
+
+export const defaultState: ImagesState = {
+  url: '',
+};
