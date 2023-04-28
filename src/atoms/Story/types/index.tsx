6a808506300147ecc5fccbe6f35d78d7b98aa12c
anthony <aalbertini95@gmail.com>
2023-04-28 12:09:28 +0200

    Add comment for TS

--- src/atoms/Story/types/index.tsx
+++ src/atoms/Story/types/index.tsx
@@ -1,3 +1,5 @@
+// See this other way: https://bobbyhadz.com/blog/typescript-element-implicitly-has-any-type-expression
+
 export type TStoryKey = 'hero' | 'villain' | 'place';
 
 export type TStory = {
